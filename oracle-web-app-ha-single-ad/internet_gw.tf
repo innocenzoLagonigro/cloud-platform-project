6c5bd7d1720865f123dc92b17171791050225d33
innocenzoLagonigro <innocenzo.lagonigro@gmail.com>
2021-07-07 15:46:20 +0200

    Removed useless files

--- oracle-web-app-ha-single-ad/internet_gw.tf
+++
@@ -1,9 +0,0 @@
-/*# Create internet gateway to allow public internet traffic
-
-resource "oci_core_internet_gateway" "ig" {
-  compartment_id = var.compartment_ocid
-  display_name   = "ig-gateway"
-  vcn_id         = oci_core_virtual_network.vcn.id
-}
-
-*/
--- oracle-web-app-ha-single-ad/natgateway.tf
+++
@@ -1,5 +0,0 @@
-/*resource "oci_core_nat_gateway" "nat_gw" {
-    compartment_id = var.compartment_ocid
-    display_name = "nat_gateway"
-    vcn_id = oci_core_virtual_network.vcn.id
-}*/
